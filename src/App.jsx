deedb24237c7b0b94e7ae3e0a6766ad90e14e786
Lucie Hélias <lucie.helias@protonmail.com>
2024-09-04 17:32:16 +0200

    create documents and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import Cookies from "js-cookie";
 
 const App = () => {
   return <></>;
