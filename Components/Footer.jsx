dec788f8e0492b6e71fcdff7aaff8210fbc3f21c
Lucie Hélias <lucie.helias@protonmail.com>
2024-09-04 18:18:46 +0200

    create footer

---
+++ Components/Footer.jsx
@@ -0,0 +1,17 @@
+import "../Styles/Footer.css";
+
+const Footer = () => {
+  return (
+    <Footer>
+      <h1>
+        Made at <a href="https://www.lereacteur.io/" target="_blank"></a>Le
+        Reacteur by
+        <a href="https://github.com/luciehelias" target="_blank">
+          Lucie Hélias
+        </a>
+      </h1>
+    </Footer>
+  );
+};
+
+export default Footer;
--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import Cookies from "js-cookie";
 
 import Header from "../Components/Header";
+import Footer from "../Components/Footer";
 
 const App = () => {
   return (
     <Router>
       <Header />
       <Routes></Routes>
+      <Footer />
     </Router>
   );
 };
