d962de77a32e495702141ce0dc70b0b9cc558762
Lucie Hélias <lucie.helias@protonmail.com>
2024-09-04 18:41:45 +0200

    create footer and css

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -2,15 +2,26 @@ import "../Styles/Footer.css";
 
 const Footer = () => {
   return (
-    <Footer>
+    <footer>
       <h1>
-        Made at <a href="https://www.lereacteur.io/" target="_blank"></a>Le
-        Reacteur by
-        <a href="https://github.com/luciehelias" target="_blank">
+        Made at {""}
+        <a
+          href="https://www.lereacteur.io/"
+          target="_blank"
+          className="header-link"
+        >
+          Le Reacteur
+        </a>{" "}
+        by {""}
+        <a
+          href="https://github.com/luciehelias"
+          target="_blank"
+          className="header-link"
+        >
           Lucie Hélias
         </a>
       </h1>
-    </Footer>
+    </footer>
   );
 };
 
