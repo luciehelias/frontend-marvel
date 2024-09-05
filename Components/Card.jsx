b95a593449566a154a5e60f1e301b685f772b635
Lucie Hélias <lucie.helias@protonmail.com>
2024-09-05 20:56:53 +0200

    create card component

---
+++ Components/Card.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import { GoHeartFill } from "react-icons/go";
+
+const Card = ({ element, name }) => {
+  return (
+    <div key={element.id}>
+      <Link to={`/${name}/${element.id}`}>
+        <div className="card">
+          <div className="card-elements">
+            <div>
+              <img src={element.thumbnail} alt={`photo du ${name}`} />
+            </div>
+            <div className="card-infos">
+              <h2>{element.name}</h2>
+              <p className="card-description">{element.description}</p>
+              <div className="card-favorite">
+                <p>Ajouter en favoris</p>
+                <GoHeartFill className="icon" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default Card;
